002f45a24b85c831dda83130b3373fdef1116fb1
Karanja Mutahi <mutahi.henry@gmail.com>
2018-04-20 01:56:58 +0300

    App Works

--- controllers/USSDcontroller.js
+++ controllers/USSDcontroller.js
@@ -1,13 +1,4 @@
-var express = require('express');
-var app = express(); 
-var bodyParser = require('body-parser');
-var options = {
-    apiKey: 'd35135d87ff9080e216f928d5366cb869d0b97fd826634ab3df8b9cbf6d5892e',
-    username: 'sandbox'
-};
-
-let USSDcontroller = function USSDcontrol(req,res){
-    
+let USSDController = function(req, res){
     let sessionID = req.body.sessionId ;
     let user = req.body.phoneNumber ;
     let text = req.body.text ;
@@ -30,17 +21,6 @@ let USSDcontroller = function USSDcontrol(req,res){
         res.end();
     }
     res.end();
-};
+}
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-//var AT = require('africastalking')(options);
-var port = process.env.PORT || 80;
-
-app.post('/mboga-ussd', USSDcontroller);
-
-app.listen(port);
-
-
-
-
+module.exports.USSDControl = USSDController;
