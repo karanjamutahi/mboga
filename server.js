4f49e4126436cc5664d7b77cc69b801cae61e2ff
Karanja Mutahi <mutahi.henry@gmail.com>
2018-04-19 23:59:23 +0300

    Added body-parser middleware

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var options = {
     apiKey: 'd35135d87ff9080e216f928d5366cb869d0b97fd826634ab3df8b9cbf6d5892e',
     username: 'sandbox'
 };
 
-var AT = require('africastalking')(options);
+app.use(bodyParser.json());
+//var AT = require('africastalking')(options);
 var port = process.env.PORT || 80;
 
-app.post('/mboga-ussd', (req, res) =>{
-    console.log(req);
+app.post('/mboga-ussd', (req, res, next) =>{
+    console.log(req.body);
     res.end();
 });
 
